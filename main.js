6e1fd4aba2041e82b5a2acce10ff4e52932fb47e
Mahir <Mahir@ASUS>
2026-01-16 06:39:12 +0300

    Fix Google Translate URL for local testing (force https)

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Inject the Google Translate Script
         const script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+        script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
         document.body.appendChild(script);
     }
 });
